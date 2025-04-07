730c571c073f691a1f79e5824798084b324ec9eb
Irvan Maulana <vanir1812@gmail.com>
2025-04-07 17:13:51 +0700

    fix: trailing slash in /api/

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { trimTrailingSlash } from "hono/trailing-slash";
 
 import { api } from "~/routes/api";
 
@@ -8,6 +9,7 @@ import { ENV } from "~/lib/types";
 const app = new Hono<{ Bindings: ENV }>();
 
 app.use(logger());
+app.use(trimTrailingSlash());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
